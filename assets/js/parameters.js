2ce50c58af8e0ca8ed90a7d0164d14fb0b06d75b
Gerardo Furtado <gerardofurtado@gmail.com>
2024-01-18 23:24:32 +1000

    Revert "2023 as default"
    This reverts commit 991b475081b7688da2e44d8dad0bb91ff28068d9.

--- assets/js/parameters.js
+++ assets/js/parameters.js
@@ -3,7 +3,7 @@ const parameters = {
 	chart: "allocationsByCountry", //chart displayed when the page loads. 
 	//The accepted values are: "allocationsByCountry", "allocationsBySector", "allocationsByType", "allocationsByMonth", "contributionsByCerfCbpf" and "contributionsByDonor"
 	fund: "total", //type of fund: total, cerf/cbpf, cerf, cbpf
-	year: 2023, //if 'null' the year will be the current year
+	year: null, //if 'null' the year will be the current year
 	showClosedFunds: false, //show or hide closed funds
 	cerfFirstYear: null, //if 'null' the first year will be the first year in the data
 	cbpfFirstYear: null, //if 'null' the first year will be the first year in the data
